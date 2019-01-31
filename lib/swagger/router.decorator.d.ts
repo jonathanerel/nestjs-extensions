ccadb1e2b40becc62461cd1fa37d3a7d7331e9be
joni <jonathan@erel.email>
2019-01-31 10:45:26 +0200

    add router decorator for semantic naming

---
+++ lib/swagger/router.decorator.d.ts
@@ -0,0 +1,7 @@
+import 'reflect-metadata';
+import { Controller } from '@nestjs/common';
+/**
+ * Defines the Router. The router can inject dependencies through constructor.
+ * Those dependencies have to belong to the same module.
+ */
+export declare const Router: typeof Controller;
